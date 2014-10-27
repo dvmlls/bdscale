0afc6ff20604fe303fa85b8b37fcea5587e979dc
David Mills <david.mills@tplustechnology.com>
2014-10-27 10:31:10 -0400

    rework the call to yahoo finance per Chuck's suggestion

--- R/yahoo_dates.R
+++ R/yahoo_dates.R
@@ -1,13 +1,14 @@
-yahoo <- function(args) {
-  base <- 'http://real-chart.finance.yahoo.com/table.csv'
-  csv <- read.csv(url(paste(base, args, sep='?')))
+#' Get past trading days using close prices of supplied ticker
+#' 
+#' @param ticker The ticker you want to use, defaults to S&P 500: \code{^GSPC}
+#' @return a vector of Date 
+#' @importFrom utils URLencode
+#' @export
+#' 
+yahoo <- function(ticker='^GSPC') {
+  encoded <- URLencode(ticker)
+  u <- sprintf('http://real-chart.finance.yahoo.com/table.csv?s=%s&g=d&a=0&b=3&c=1950&ignore=.csv', encoded)
+  csv <- read.csv(url(u))
   
   sort(as.Date(csv$Date))
 }
-
-#' Get past trading days for NYSE using close prices of SPX
-#' 
-#' @return a vector of Date 
-#' @export
-#' 
-get_nyse <- function() yahoo('s=%5EGSPC&d=9&e=8&f=2014&g=d&a=0&b=3&c=1950&ignore=.csv')
