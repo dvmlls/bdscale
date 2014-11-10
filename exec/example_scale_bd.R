45af2c49891618dbd240bdfce9cb4bc06b614c44
David Mills <david.mills@tplustechnology.com>
2014-11-10 10:33:46 -0500

    remove magrittr and dplyr dependencies from example

--- exec/example_scale_bd.R
+++ exec/example_scale_bd.R
@@ -1,16 +1,14 @@
-## Not run:
-
-require(dplyr, quietly=TRUE, warn.conflicts=FALSE)
 require(ggplot2, quietly=TRUE)
 require(scales)
+require(bdscale)
 
 nyse <- yahoo()
 
 set.seed(12345)
 df <- data.frame(date=nyse, price=cumsum(rnorm(length(nyse))) + 100)
+df <- subset(df, as.Date('2014-08-01') <= date & date <= as.Date('2014-10-08')) 
 
-plot <- df %>% filter(as.Date('2014-08-01') <= date & date <= as.Date('2014-10-08')) %>% 
-  ggplot(aes(x=date, y=price)) + geom_step() + 
+plot <- ggplot(df, aes(x=date, y=price)) + geom_step() + 
   theme(axis.title.x=element_blank(), axis.title.y=element_blank())
 
 plot + ggtitle('calendar dates')
@@ -27,7 +25,7 @@ plot + scale_x_bd(business.dates=nyse, max.major.breaks=10, labels=date_format('
 
 # ggsave(file='man/figures/business.week.PNG', width=6, height=2)
 
-options <- c('2014-08-15', '2014-09-19') %>% as.Date
+options <- as.Date(c('2014-08-15', '2014-09-19'))
 
 plot + 
   geom_vline(xintercept=as.numeric(options), size=2, alpha=0.25) + 
@@ -41,5 +39,3 @@ plot +
   ggtitle('business dates, option expiry')
 
 # ggsave(file='man/figures/business.options.PNG', width=6, height=2)
-
-## End(Not run)
